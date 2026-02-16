efa1c3b184a8adc1c510e575daf1b04fff65b517
jesusMartinez88 <jesusmartinezsanchez9@gmail.com>
2026-02-16 20:31:56 +0100

    feature: new fotos

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -16,12 +16,17 @@ interface Photo {
 })
 export class GalleryComponent {
   photos: Photo[] = [
-    { id: 1, title: 'Momento especial 1', placeholder: '📷' },
-    { id: 2, title: 'Momento especial 2', placeholder: '📷' },
-    { id: 3, title: 'Momento especial 3', placeholder: '📷' },
-    { id: 4, title: 'Momento especial 4', placeholder: '📷' },
-    { id: 5, title: 'Momento especial 5', placeholder: '📷' },
-    { id: 6, title: 'Momento especial 6', placeholder: '📷' }
+    { id: 1, title: 'Momento especial 1', placeholder: 'assets/fotos/foto1.jpeg' },
+    { id: 2, title: 'Momento especial 2', placeholder: 'assets/fotos/foto2.jpeg' },
+    { id: 3, title: 'Momento especial 3', placeholder: 'assets/fotos/foto3.jpeg' },
+    { id: 4, title: 'Momento especial 4', placeholder: 'assets/fotos/foto4.jpeg' },
+    { id: 5, title: 'Momento especial 5', placeholder: 'assets/fotos/foto5.jpeg' },
+    { id: 6, title: 'Momento especial 6', placeholder: 'assets/fotos/foto6.jpeg' },
+    { id: 6, title: 'Momento especial 6', placeholder: 'assets/fotos/foto7.jpeg' },
+    { id: 6, title: 'Momento especial 6', placeholder: 'assets/fotos/foto8.jpeg' },
+    { id: 6, title: 'Momento especial 6', placeholder: 'assets/fotos/foto9.jpeg' },
+    { id: 6, title: 'Momento especial 6', placeholder: 'assets/fotos/foto10.jpeg' },
+    { id: 6, title: 'Momento especial 6', placeholder: 'assets/fotos/foto11.jpeg' },
   ];
 
   selectedPhoto: Photo | null = null;
